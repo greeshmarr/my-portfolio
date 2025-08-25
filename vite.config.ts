c289791baf18c79ee62175774b2bd06f13f10ff6
greeshmarr <greeshmarr23@gmail.com>
2025-08-25 12:05:18 +0530

    Support github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/', // Add this for GitHub Pages deployment
   resolve: {
     alias: {
       "@": "/src",
